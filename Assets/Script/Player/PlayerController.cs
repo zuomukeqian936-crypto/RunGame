6d2af13dd55385847dc81ffc4b670bb2c01a0e0e
zuomukeqian936-crypto <zuomukeqian936@gmail.com>
2026-09-19 12:18:39 +0900

    [fix]プレハブの編集

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -5,10 +5,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _forwardSpeed = 5f;       // 前進スピード
     [SerializeField] private float _laneChangeSpeed = 15f;   // レーン移動の速さ
     [SerializeField] private float[] _laneXPositions = { -8f, -5f, -2f }; // 左, 真ん中, 右のX座標
+    [SerializeField] private float _jumpPower = 3f;
 
     private Rigidbody _rigidbody;
     private PlayerPosition _currentPosition = PlayerPosition.Middle;
-    private bool _isPressed = false;
+    private Vector3 _playerDirection;
+    private bool _isRotate = false;
+    private bool _isJump = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,6 +21,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         HandleLaneInput();
+        HandleJumpInput();
     }
 
     // Update is called once per frame
@@ -34,67 +38,117 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             // 左に移動できる場合（Middle -> Left、Right -> Middle）
-            if (_currentPosition > PlayerPosition.Left)
-            {
+            if (_currentPosition > PlayerPosition.Left && !_isRotate)
+            { 
                 _currentPosition--;
-                _isPressed = true;
+                _isRotate = true;
+                PlayerRotate();
             }
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && !_isRotate)
         {
             // 右に移動できる場合（Left -> Middle、Middle -> Right）
             if (_currentPosition < PlayerPosition.Right)
             {
                 _currentPosition++;
-                _isPressed = true;
+                _isRotate = true;
+                PlayerRotate();
             }
         }
     }
 
     /// <summary>
-    /// プレイヤー前進処理
+    /// ジャンプ処理
+    /// </summary>
+    private void HandleJumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !_isJump)
+        {
+            _rigidbody.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+            _isJump = true;
+            Debug.Log("ジャンプしました");
+        }
+    }
+
+    /// <summary>
+    /// プレイヤー方向転換処理
+    /// </summary>
+    private void PlayerRotate()
+    {
+        // 1. 目標のX座標をenumのインデックスから取得
+        float targetX = _laneXPositions[(int)_currentPosition];
+        float currentX = _rigidbody.position.x;
+
+        // 2. 現在のX座標から目標のX座標へ滑らかに補間移動
+        _playerDirection = new Vector3(0f, targetX - currentX, 0f).normalized;
+        transform.Rotate(_playerDirection);
+    }
+
+    /// <summary>
+    /// プレイヤーの移動処理
     /// </summary>
     private void MovePlayer()
     {
-        if(_isPressed == true)
+        // 1. 目標のX座標をenumのインデックスから取得
+        float targetX = _laneXPositions[(int)_currentPosition];
+        float currentX = _rigidbody.position.x;
+
+        float newX = Mathf.MoveTowards(currentX, targetX, _laneChangeSpeed * Time.fixedDeltaTime);
+
+        // 3. 前進（Z方向）とレーン移動（X方向）を合わせた移動ベクトルを作成
+        Vector3 moveVelocity = new Vector3((newX - currentX) / Time.fixedDeltaTime, _rigidbody.linearVelocity.y, _forwardSpeed);
+
+        _rigidbody.linearVelocity = moveVelocity;
+
+        if (Mathf.Abs(targetX - currentX) < 0.1f && _isRotate)
         {
-            // 1. 目標のX座標をenumのインデックスから取得
-            float targetX = _laneXPositions[(int)_currentPosition];
-
-            // 2. 現在のX座標から目標のX座標へ滑らかに補間移動
-            float currentX = _rigidbody.position.x;
-            float newX = Mathf.MoveTowards(currentX, targetX, _laneChangeSpeed * Time.fixedDeltaTime);
-
-            // 3. 前進（Z方向）とレーン移動（X方向）を合わせた移動ベクトルを作成
-            Vector3 moveVelocity = new Vector3((newX - currentX) / Time.fixedDeltaTime, _rigidbody.linearVelocity.y, _forwardSpeed);
-
-            _rigidbody.linearVelocity = moveVelocity;
-
-            if(Mathf.Approximately(targetX,newX))
-            {
-                ResetPress();
-            }
+            transform.Rotate(Vector3.zero);
+            _isRotate = false;
         }
-        else
+    }
+
+
+        //if(_isPressed == true)
+        //{
+        //    // 1. 目標のX座標をenumのインデックスから取得
+        //    float targetX = _laneXPositions[(int)_currentPosition];
+
+        //    // 2. 現在のX座標から目標のX座標へ滑らかに補間移動
+        //    float currentX = _rigidbody.position.x;
+        //    _playerDirection = new Vector3(0f, currentX - targetX, 0f).normalized;
+        //    transform.Rotate(_playerDirection);
+
+        //    float newX = Mathf.MoveTowards(currentX, targetX, _laneChangeSpeed * Time.fixedDeltaTime);
+
+        //    // 3. 前進（Z方向）とレーン移動（X方向）を合わせた移動ベクトルを作成
+        //    Vector3 moveVelocity = new Vector3((newX - currentX) / Time.fixedDeltaTime, _rigidbody.linearVelocity.y, _forwardSpeed);
+
+        //    _rigidbody.linearVelocity = moveVelocity;
+
+        //    if(Mathf.Approximately(targetX,newX))
+        //    {
+        //        ResetPress();
+        //    }
+        //}
+        //else
+        //{
+        //    if(_playerDirection != Vector3.zero)
+        //    {
+        //        transform.Rotate(new Vector3(0f, 0f, 0f));
+        //    }
+
+        //    _rigidbody.linearVelocity = new Vector3(0f, 0f, _forwardSpeed);
+        //}  
+
+    /// <summary>
+    /// 着地処理
+    /// </summary>
+    /// <param name="other">接触したオブジェクト</param>
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
         {
-            _rigidbody.linearVelocity = new Vector3(0f, 0f, _forwardSpeed);
-        }   
-    }
-
-    /// <summary>
-    /// スピード加算処理
-    /// </summary>
-    /// <param name="applyForce">加算率</param>
-    public void ApplyForce(float applyForce = 1.1f)
-    {
-        _forwardSpeed *= applyForce;
-    }
-
-    /// <summary>
-    /// 方向移動完了処理
-    /// </summary>
-    private void ResetPress()
-    {
-        _isPressed = false;
+            _isJump = false;
+        }
     }
 }
